edb6dba7897094f6866578c0cc90e962f954dce7
vhtmui_5600g <798088486@qq.com>
2024-04-23 00:40:50 +0800

    html learning

--- Root/scripts/main.js
+++ Root/scripts/main.js
@@ -1,4 +1,29 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+let myImage = document.querySelector("img");
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/1.jpg") {
+        myImage.setAttribute("src", "images/2.png");
+    } else {
+        myImage.setAttribute("src", "images/1.jpg");
+    }
+};
 
-  
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+        setUserName();
+    }
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+}
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+}
+myButton.onclick = function () {
+    setUserName();
+};
