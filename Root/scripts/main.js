39e90520b067dddffc409b167959013a7a02c655
15363205631 <q.2@foxmail.com>
2024-07-18 18:00:19 +0800

    1

--- Root/scripts/main.js
+++ Root/scripts/main.js
@@ -1,29 +1,8 @@
-let myImage = document.querySelector("img");
-myImage.onclick = function () {
-    let mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/1.jpg") {
-        myImage.setAttribute("src", "images/2.png");
-    } else {
-        myImage.setAttribute("src", "images/1.jpg");
-    }
-};
+const para = document.querySelector("p");
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-function setUserName() {
-    let myName = prompt("请输入你的名字。");
-    if (!myName) {
-        setUserName();
-    }
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+para.addEventListener("click", updateName);
+
+function updateName() {
+  const name = window.prompt("Enter a new name");
+  para.textContent = `Player 1: ${name}`;
 }
-if (!localStorage.getItem("name")) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
-}
-myButton.onclick = function () {
-    setUserName();
-};
