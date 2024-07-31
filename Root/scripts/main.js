62903494926c9d14f14c16b77afcf8c84ab75a6b
15363205631 <q.2@foxmail.com>
2024-07-31 18:00:30 +0800

    1

--- Root/scripts/main.js
+++ Root/scripts/main.js
@@ -1,9 +1,37 @@
-const imgFile = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"]
+const section = document.querySelector('section');
 
-class Shape {
+let para1 = document.createElement('p');
+let para2 = document.createElement('p');
+let motherInfo = 'The mother cats are called ';
+let kittenInfo;
+const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json';
 
-    name;
-    sides;
-    sideLength;
-  
-  }
+fetch(requestURL)
+    .then(response => response.text())
+    .then(text => displayCatInfo(text))
+
+function displayCatInfo(catString) {
+    let total = 0;
+    let male = 0;
+
+    // Add your code here
+    const a = JSON.parse(catString);
+    const mothers = new Array;
+    for (const key in a) {
+        mothers.push(a[key].name);
+        a[key].kittens.forEach(element => {
+            total += 1;
+            if (element.gender === 'm') {
+                male += 1;
+            }
+        });
+    }
+    motherInfo += mothers.join(', ');
+    // Don't edit the code below here!
+    kittenInfo = `The total kittens is ${total} The number of male is ${male}`
+    para1.textContent = motherInfo;
+    para2.textContent = kittenInfo;
+}
+
+section.appendChild(para1);
+section.appendChild(para2);
