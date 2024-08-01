e03020533256f812374be04b972dcca841d77a84
15363205631 <q.2@foxmail.com>
2024-08-01 18:00:30 +0800

    1

--- Root/scripts/main.js
+++ Root/scripts/main.js
@@ -1,37 +1,29 @@
-const section = document.querySelector('section');
-
-let para1 = document.createElement('p');
-let para2 = document.createElement('p');
-let motherInfo = 'The mother cats are called ';
-let kittenInfo;
-const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/tasks/json/sample.json';
-
-fetch(requestURL)
-    .then(response => response.text())
-    .then(text => displayCatInfo(text))
-
-function displayCatInfo(catString) {
-    let total = 0;
-    let male = 0;
-
-    // Add your code here
-    const a = JSON.parse(catString);
-    const mothers = new Array;
-    for (const key in a) {
-        mothers.push(a[key].name);
-        a[key].kittens.forEach(element => {
-            total += 1;
-            if (element.gender === 'm') {
-                male += 1;
-            }
-        });
+function generatePrimes(quota) {
+    function isPrime(n) {
+      for (let c = 2; c <= Math.sqrt(n); ++c) {
+        if (n % c === 0) {
+          return false;
+        }
+      }
+      return true;
     }
-    motherInfo += mothers.join(', ');
-    // Don't edit the code below here!
-    kittenInfo = `The total kittens is ${total} The number of male is ${male}`
-    para1.textContent = motherInfo;
-    para2.textContent = kittenInfo;
-}
-
-section.appendChild(para1);
-section.appendChild(para2);
+    const primes = [];
+    const maximum = 1000000;
+    while (primes.length < quota) {
+      const candidate = Math.floor(Math.random() * (maximum + 1));
+      if (isPrime(candidate)) {
+        primes.push(candidate);
+      }
+    }
+    return primes;
+  }
+  document.querySelector("#generate").addEventListener("click", () => {
+    const quota = document.querySelector("#quota").value;
+    const primes = generatePrimes(quota);
+    document.querySelector("#output").textContent =
+      `完成！已生成素数${quota}个。`;
+  });
+  document.querySelector("#reload").addEventListener("click", () => {
+    document.location.reload();
+  });
+  
