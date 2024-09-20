f2cae29fc060f4eea5f936be42775c96f02a32b3
15363205631 <q.2@foxmail.com>
2024-09-20 18:14:02 +0800

    1

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,6 +1,7 @@
 <script>
-	import { afterUpdate, beforeUpdate } from 'svelte';
+	import { afterUpdate, beforeUpdate, onMount } from 'svelte';
 	import { get_child_array } from '../routes/nav';
+	import { url } from './stores';
 	import Icon from './Icon.svelte';
 
 	export let tree;
@@ -10,7 +11,6 @@
 	let child_tree = get_child_array(tree);
 	let option = 'chevron_down';
 	let up = false;
-	let url;
 
 	let nowLink = preLink + '/' + tree.$link;
 
@@ -18,27 +18,32 @@
 		display = !display;
 		up = !up;
 	}
-	afterUpdate(()=>{
-		url = window.location.href
-	})
+
+	function update_url() {
+		beforeUpdate(() => {
+			console.log(window.location.href);
+			url.set(window.location.href);
+		});
+	}
 </script>
-{url}
+
+{$url}
 {#if tree}
 	<div class="tree-head">
-		
 		{#if child_tree}
 			<button class:up on:click={toggle_display}><Icon {option} /></button>
-			<a class="sidebar" href={nowLink}>{tree.$title}</a>
+			<a class="sidebar" href={nowLink} on:click={update_url}>{tree.$title}</a>
 		{:else}
-			<div class = 'sidebar-paddingblock'></div><a class="sidebar" href={nowLink}>{tree.$title}</a>
+			<div class="sidebar-paddingblock"></div>
+			<a class="sidebar" href={nowLink} on:click={update_url}>{tree.$title}</a>
 		{/if}
-		
 	</div>
 
 	{#if display && child_tree}
 		<ul>
 			{#each child_tree as t}
-				<div class = 'sidebar-paddingblock'> </div><li><svelte:self tree={t} preLink={nowLink} /></li>
+				<div class="sidebar-paddingblock"></div>
+				<li><svelte:self tree={t} preLink={nowLink} /></li>
 			{/each}
 		</ul>
 	{/if}
@@ -48,13 +53,12 @@
 	ul {
 		list-style: none;
 		margin: 0 0 0 1.125rem; /* 侧边栏关联样式，保证svg图像对齐边框*/
-		padding-left: 0.0;
+		padding-left: 0;
 		border-collapse: collapse;
 		border-left: 1px solid var(--sidebar-border-left-color);
 		/* border-top: 1px solid var(--sidebar-border-top-color); */
 		/* border-bottom: 1px solid; */
 		& li {
-			
 		}
 	}
 	a.sidebar {
@@ -64,11 +68,11 @@
 			text-decoration: underline;
 		}
 	}
-	div.tree-head{
+	div.tree-head {
 		display: flex;
 		line-height: 3rem;
 	}
-	div.sidebar-paddingblock{
+	div.sidebar-paddingblock {
 		display: inline;
 		width: 2.25rem;
 		/* 侧边栏关联样式，保证列表每项文字对齐 */
@@ -76,14 +80,14 @@
 	button {
 		box-sizing: border-box;
 		margin: 0;
-		padding: 0 0.5rem 0 0.5rem;	
-		height: var(--header-block-height);	
+		padding: 0 0.5rem 0 0.5rem;
+		height: var(--header-block-height);
 		border: none;
 		outline: none;
 		background-color: transparent;
 		transition: cubic-bezier(0.01, 0.79, 0.45, 0.94) 0.25s;
 		transform: rotate(-90deg);
-		& svg{
+		& svg {
 			vertical-align: middle;
 		}
 		& path {
