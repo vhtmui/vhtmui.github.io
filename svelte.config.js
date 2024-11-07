7d5ea49f8c9ba9751b9d55596ea26094f6ff5aa3
15363205631 <q.2@foxmail.com>
2024-11-07 10:28:20 +0800

    1

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: false,
-			precompress: false,
-			strict: true
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: false,
+			// precompress: false,
+			// strict: true
 		}),
 		prerender: {
 			//忽略检查
