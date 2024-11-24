6a6b226f6ea8006e097c8e0c3d019ba241a1457c
vhtmui_5600g <q.2@foxmail.com>
2024-11-24 22:09:44 +0800

    1

---
+++ src/lib/ZCode/ZCode.svelte
@@ -0,0 +1,10 @@
+<script>
+	import ZIcon from "$lib/ZIcon/ZIcon.svelte";
+
+	let { children } = $props();
+</script>
+
+<div>
+	<code> {children}</code>
+    <div><ZIcon option="copy"></ZIcon></div>
+</div>
--- src/routes/docs/[doc]/+page.svelte
+++ src/routes/docs/[doc]/+page.svelte
@@ -2,21 +2,37 @@
 	import Markdown from 'svelte-exmarkdown';
 	import { gfmPlugin } from 'svelte-exmarkdown/gfm';
 	import rehypeSlug from 'rehype-slug';
+	// import rehypeHighlight from 'rehype-highlight';
+	// import powershell from 'highlight.js/lib/languages/powershell';
+	// import { common } from 'lowlight';
+	// import 'highlight.js/styles/github.css';
+
+	import ZCode from '$lib/ZCode/ZCode.svelte';
 
 	/** @type {import('./$types').PageData} */
 	let { data } = $props();
 
-	const headingIdPlugin = {
-		rehypePlugin: [rehypeSlug]
-	};
-
 	let md = $state('# hello world');
-	const plugins = [gfmPlugin(), headingIdPlugin];
-
+	const plugins = [
+		{
+			
+			rehypePlugin: [rehypeSlug],
+			// renderer: { code: ZCode }
+		},
+		// {
+		// 	rehypePlugin: [rehypeHighlight, { languages: { ...common, powershell } }]
+		// },
+		gfmPlugin()
+	];
+	$inspect(data.content)
 </script>
 
 <Markdown md={data.content} {plugins} />
 
 <style>
-
+	:global {
+		code {
+			font-family: 'CascadiaMono';
+		}
+	}
 </style>
