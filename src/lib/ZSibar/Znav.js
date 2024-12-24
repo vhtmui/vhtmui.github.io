98324b2d6d6d56f668afa2ffa07621873fda2e27
vhtmui <798088486@qq.com>
2024-12-24 11:49:18 +0800

    new Tree class

--- src/lib/ZSibar/Znav.js
+++ src/lib/ZSibar/Znav.js
@@ -5,32 +5,77 @@
  * @property {Tree} [*] - Optional children nodes. Each child must also be a Tree.
  */
 class Tree {
-    _title = '/';
-    _link = '/';
-    children = [];
+    #title = '/';
+    #link = '/';
+    #children = [];
     constructor(dirArray) {
         if (dirArray) {
             dirArray.forEach((dir) => {
                 let currentNode = this;
                 let parts = dir.split('/').filter(Boolean);
                 parts.forEach((part, i) => {
-                    let index = currentNode.children.findIndex((v) => v._title === part);
+                    let index = currentNode.#children.findIndex((v) => v.#title === part);
                     if (index === -1) {
-                        currentNode.children.push({
-                            _title: part,
-                            _link: this._link + parts.slice(0, i + 1).join('/'),
-                            children: []
-                        });
-                        currentNode = currentNode.children[0];
+                        const newTree = new Tree();
+                        newTree.#title = part;
+                        newTree.#link = '/' + parts.slice(0, i + 1).join('/');
+                        currentNode.#children.push(newTree);
+                        currentNode = newTree;
                     } else {
-                        currentNode = currentNode.children[index];
+                        currentNode = currentNode.#children[index];
                     }
-                })
-            })
+                });
+            });
         }
     }
+    get title() {
+        return this.#title;
+    }
+    set title(title) {
+        this.#title = title;
+    }
+    get link() {
+        return this.#link;
+    }
+    set link(link) {
+        this.#link = link;
+    }
+    get children() {
+        // 返回新的数组，禁止直接修改children
+        return [...this.#children];
+    }
+    /**
+     * Returns a set of all the titles in the tree, including the current node's title.
+     * @returns {Set<string>} A set of all the titles in the tree.
+     */
+    getAllChildTitle() {
+        let result = [];
+        if (this.#children.length > 0) {
+            this.#children.forEach((child) => result.push(...child.getAllChildTitle(), this.#title));
+        } else {
+            result.push(this.#title);
+        }
+        return new Set(result);
+    }
+    #getPubObject() {
+        let pubObj = { link: this.#link, title: this.#title, children: [] };
+        if (this.#children.length > 0) {
+            pubObj.children = this.#children.map((tree) => tree.#getPubObject());
+        }
+        return pubObj;
+    }
+    /**
+     * Returns the JSON string representation of the tree.
+     * @param {Function|null} replacer — A function that transforms the results.
+     * @param {number} space — Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+     * @returns {string}
+     */
+    toString(replacer = null, space = 4) {
+        if (arguments.length === 1)
+            return JSON.stringify(this.#getPubObject(), null, arguments[0])
+        return JSON.stringify(this.#getPubObject(), replacer, space);
+    }
 }
-
 /**
  * Return child objects from a parent object, excluding objects whose names start with string '_'
  * @param {Tree} obj
