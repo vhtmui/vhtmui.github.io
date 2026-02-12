43f6d05284cba396605e106b29687b7d2c539348
vhtmui <790888486@qq.com>
2026-02-12 15:29:16 +0800

    feat: add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import prettier from 'eslint-config-prettier';
 import path from 'node:path';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
@@ -11,6 +12,8 @@ const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } }
 	},
--- src/lib/myUtils.js
+++ src/lib/myUtils.js
@@ -1,4 +1,4 @@
-import { onMount } from "svelte";
+import { onMount } from 'svelte';
 
 /**
  * Description placeholder
@@ -8,35 +8,35 @@ import { onMount } from "svelte";
  * @param {function} handler
  */
 export function useShortCut(shortcut, handler) {
-  shortcut = shortcut.toLowerCase();
-  if (shortcut.split("+").length !== 2)
-    throw new Error("Invalid shortcut format. Please use 'modifier+key'.");
+	shortcut = shortcut.toLowerCase();
+	if (shortcut.split('+').length !== 2)
+		throw new Error("Invalid shortcut format. Please use 'modifier+key'.");
 
-  onMount(() => {
-    /**
-     * @param {KeyboardEvent} event
-     */
-    function handleKeyDown(event) {
-      const [modifier, key] = shortcut.split("+");
+	onMount(() => {
+		/**
+		 * @param {KeyboardEvent} event
+		 */
+		function handleKeyDown(event) {
+			const [modifier, key] = shortcut.split('+');
 
-      const isModifierPressed =
-        (modifier === "ctrl" && event.ctrlKey) ||
-        (modifier === "alt" && event.altKey) ||
-        (modifier === "shift" && event.shiftKey) ||
-        (modifier === "meta" && event.metaKey); // Meta 对应 Cmd 键（Mac）
+			const isModifierPressed =
+				(modifier === 'ctrl' && event.ctrlKey) ||
+				(modifier === 'alt' && event.altKey) ||
+				(modifier === 'shift' && event.shiftKey) ||
+				(modifier === 'meta' && event.metaKey); // Meta 对应 Cmd 键（Mac）
 
-      const isTargetKeyPressed = event.key.toLowerCase() === key;
+			const isTargetKeyPressed = event.key.toLowerCase() === key;
 
-      if (isModifierPressed && isTargetKeyPressed) {
-        event.preventDefault();
-        handler();
-      }
-    }
+			if (isModifierPressed && isTargetKeyPressed) {
+				event.preventDefault();
+				handler();
+			}
+		}
 
-    window.addEventListener("keydown", handleKeyDown);
+		window.addEventListener('keydown', handleKeyDown);
 
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  });
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	});
 }
