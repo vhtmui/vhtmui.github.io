a9cd139d2c2395703fec283a82d80f2f339765b9
vhtmui <798088486@qq.com>
2026-02-25 21:05:47 +0800

    add  sidebar component

---
+++ src/lib/components/ui/kbd/kbd-group.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { cn } from "$lib/utils.js";
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	} = $props();
+</script>
+
+<kbd
+	bind:this={ref}
+	data-slot="kbd-group"
+	class={cn("inline-flex items-center gap-1", className)}
+	{...restProps}
+>
+	{@render children?.()}
+</kbd>
