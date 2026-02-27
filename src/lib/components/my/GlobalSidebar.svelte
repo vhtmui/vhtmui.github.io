e64bec3d4ed5e69d28f9a34478a545c784d4a1e0
vhtmui <790888486@qq.com>
2026-02-27 17:59:44 +0800

    1

--- src/lib/components/my/GlobalSidebar.svelte
+++ src/lib/components/my/GlobalSidebar.svelte
@@ -1,13 +1,11 @@
 <script module>
-	import { setContext } from 'svelte';
+	import { setContext, getContext } from 'svelte';
+	const GSBCONTEXT = Symbol('GSB');
+
 	/**
 	 * 侧边栏菜单项，基于路径渲染
-	 *
-	 * @abstract sadfsdfsf
 	 */
 	export class GlobalSidebarMenuItem {
-		static a = 1;
-		b = 2;
 		/**
 		 * @param {string} path 路径
 		 * @param {string} label 显示名称
@@ -16,44 +14,72 @@
 		 * @param {boolean} open 是否展开
 		 * @param {boolean} highlight 是否高亮
 		 */
-		constructor(path, label, href, icon, open = false, highlight) {
+		constructor(path, label, href, icon = null, open = false, highlight = false) {
 			this.path = path;
 			this.label = label;
 			this.href = href;
 			this.icon = icon;
 			this.open = open;
 			this.highlight = highlight;
+			this.children = [];
 		}
 	}
 
-	class GlobalSidebarManager {
-		items = new Map();
-		/**
-		 * @description 侧边栏菜单
-		 *
-		 * @param {GlobalSidebarMenuItem[]} sbMenuItems 菜单项
-		 */
-		constructor(sbMenuItems) {
-			sbMenuItems.forEach((item) => {
-				this.items.set(item.path, item);
-			});
-		}
+	/**
+	 * @description 创建菜单项
+	 * @param {GlobalSidebarMenuItem[]} itemProps 菜单项
+	 * @returns {Map<string, GlobalSidebarMenuItem>} 菜单项
+	 */
+	function createSidebarMenuItems(itemProps) {
+		let items = new Map();
+		itemProps.forEach((item) => {
+			items.set(item.path, item);
+			let parentPath = item.path.split('/').slice(0, -1).join('/');
+			if (parentPath && items.has(parentPath)) {
+				let parent = items.get(parentPath);
+				parent.children.push(item);
+			}
+		});
+		return items;
 	}
 
 	/**
 	 * @function setSidebarManager
 	 * @description 设置侧边栏管理器
-	 * @param {GlobalSidebarManager} manager 侧边栏管理器
+	 * @param {GlobalSidebarMenuItem[]} itemProps 侧边栏菜单项
 	 */
-	export function setSidebarManager(manager) {
-		setContext('sidebar', manager);
+	export function setSidebarManager(itemProps) {
+		setContext(GSBCONTEXT, createSidebarMenuItems(itemProps));
+	}
+
+	export function getSidebarManager() {
+		return getContext(GSBCONTEXT);
 	}
 </script>
 
 <script>
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { House } from '@lucide/svelte';
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+
+	const menuItems = getSidebarManager();
 </script>
+
+{#snippet mitem()}
+	<Collapsible.Root open class="group/collapsible">
+		<Sidebar.MenuItem>
+			<Collapsible.Trigger>
+				{#snippet child({ props })}
+					<Sidebar.MenuButton {...props} />
+				{/snippet}
+			</Collapsible.Trigger>
+			<Collapsible.Content>
+				<Sidebar.MenuSub>
+					<Sidebar.MenuSubItem />
+				</Sidebar.MenuSub>
+			</Collapsible.Content>
+		</Sidebar.MenuItem>
+	</Collapsible.Root>
+{/snippet}
 
 <Sidebar.Root>
 	<Sidebar.Content>
