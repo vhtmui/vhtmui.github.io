557f221a34dcaff29047aec43607358b18e3bf9b
vhtmui <798088486@qq.com>
2026-03-10 21:40:16 +0800

    1

--- src/routes/docs2/+layout.server.js
+++
@@ -1,8 +0,0 @@
-import { walkDir } from '$lib/my-utils';
-
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    const docItems = await walkDir('docs', undefined, { includeRoot: true })
-
-    return { docItems };
-}
