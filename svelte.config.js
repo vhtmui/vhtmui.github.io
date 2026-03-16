965215cbc531d550610dfe8f8e4c4e8cad55a37c
vhtmui <798088486@qq.com>
2026-03-16 18:26:30 +0800

    1

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 
-// const base = process.argv.includes('dev') ? '' : '/vhtmui.github.io';
-export const base = '/vhtmui.github.io';
+const base = process.argv.includes('dev') ? '' : '/vhtmui.github.io';
+// export const base = '/vhtmui.github.io';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
